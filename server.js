adc1498842f0a282149ab7f064437ffd61a0ac9f
neutro <riteekgore37@gmail.com>
2026-02-20 14:45:46 +0530

    Simplify MongoDB connection code

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // ================= DATABASE CONNECTION =================
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ MongoDB Connected"))
 .catch((err) => console.log("❌ MongoDB Error:", err.message));
 
